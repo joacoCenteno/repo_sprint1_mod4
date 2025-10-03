7feba74407b2aaee37fcf0802858529f821742f5
joacoCenteno <centenojoaco01@gmail.com>
2025-10-03 19:50:45 -0300

    Se implementó nuevo componente para encapsular todo el layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Header from './Header'
+
+const Layout = () => {
+  return (
+      
+    <>
+        <div className='font-display'> 
+          <NavBar/>
+          <Header/>
+         </div>
+    </>
+
+  )
+}
+
+export default Layout
